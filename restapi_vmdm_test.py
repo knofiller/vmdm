a5646d511d988acb49efcf3611888c465f1a5e19
Kevin Nguyen <kevin.nguyen@emc.com>
2017-03-14 09:49:05 -0700

    basic

--- restapi_vmdm_test.py
+++ restapi_vmdm_test.py
@@ -1,6 +1,7 @@
 import requests
 from requests.packages.urllib3.exceptions import *
 requests.packages.urllib3.disable_warnings()
+import json
 
 headers = {
 	'Accept': 'application/json',
@@ -15,6 +16,18 @@ payload = {
 
 url = 'https://10.25.80.114:8443/api/v1/login'
 
-r = requests.post(url, headers=headers, data=payload, verify=False)
+r = requests.post(url, headers=headers, data=json.dumps(payload), verify=False)
 
-print(r.headers)
+print(r.status_code)
+print(r.content)
+
+res = json.loads(r.content)
+
+headers['X-Auth-Token'] = res['x-auth-token']
+
+url = 'https://10.25.80.114:8443/api/v1/historicalDiscoveryMetrics'
+
+r = requests.get(url,headers=headers, verify=False)
+
+print(r.status_code)
+print(r.content)
